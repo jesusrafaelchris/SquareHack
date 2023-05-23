a4c54cb5b8aa5dd2dce62a1c9f051304fd92a3b0
Christian Grinling <grinlingc@gmail.com>
2023-05-23 12:46:45 +0100

    Added in change displayname request

--- SquareHack/Login+Signup/SignUpViewModel.swift
+++ SquareHack/Login+Signup/SignUpViewModel.swift
@@ -25,6 +25,7 @@ class SignUpViewModel: SignUpViewModelProtocol {
             
             if let user = user {
                 // Create user document
+                self.changeDisplayName(forUID: user.uid, to: name)
                 self.authCoordinator?.createUserDocument(uid: user.uid, name: name) { error in
                     // Check for error when creating document
                     if error != nil {
@@ -37,4 +38,21 @@ class SignUpViewModel: SignUpViewModelProtocol {
             }
         })
     }
+
+    func changeDisplayName(forUID uid: String, to displayName: String) {
+        let userRef = Auth.auth().currentUser?.uid == uid ? Auth.auth().currentUser?.createProfileChangeRequest() : nil
+
+        userRef?.displayName = displayName
+        
+        userRef?.commitChanges { error in
+            if let error = error {
+                // An error occurred while updating the display name
+                print("Error updating display name: \(error.localizedDescription)")
+            } else {
+                // Display name updated successfully
+                print("Display name updated successfully")
+            }
+        }
+    }
+
 }
